0d61a8f23dd2b8866ea4aeb49a0e8b53a318a7dd
Siddharth Santosh Shinde <142812176+sid1514@users.noreply.github.com>
2024-03-01 12:10:39 +0530

    errors in fetching

--- app.js
+++ app.js
@@ -2,29 +2,20 @@ const express=require("express");
 const route=require("./route.js")
 const cors=require("cors")
 const app=express();
-const Login=require("./login")
+
 const dotenv=require("dotenv")
 dotenv.config({path:"./config.env"});
 const port=process.env.port;
 
 
 app.use(express.json())
-app.use(cors({
-    origin:"https://localhost:4000",
-    credentials:true
-}))
+app.use(cors())
 app.use(route)
-app.use('/uploads', express.static('uploads'));
 
 
 
 
-    
-  
-            
-  
-  
-  
+
   
   
 
